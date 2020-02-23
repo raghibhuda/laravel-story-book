a1a9a92dc3efac210529b41bc979b79fc7bf62f3
Ragib Ahsanul <raghibhuda@gmail.com>
2020-02-23 21:13:51 +0600

    Basic crud functionality added in controller

--- app/Http/Controllers/StoryController.php
+++ app/Http/Controllers/StoryController.php
@@ -32,8 +32,36 @@ class StoryController extends Controller
      */
     public function store(Request $request) {
         Story::create($request->all());
-        return redirect('/stories')->with('success', 'Show is successfully saved');
+        return redirect('/stories')->with('success', 'Story is successfully saved');
     }
 
+    /**
+     * @param $id
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function edit ($id) {
+        $story = Story::find($id);
+        return view('edit')->with($story);
+    }
+
+    /**
+     * @param Request $request
+     * @param $id
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
+     */
+    public function update (Request $request,$id) {
+        Story::whereId($id)->update($request->all());
+        return redirect('/stories')->with('success', 'Story is successfully saved');
+    }
+
+    /**
+     * @param $id
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
+     */
+    public function destroy($id) {
+        $story = Story::findOrfail($id);
+        $story->delete();
+        return redirect('/stories')->with('success', 'Story is successfully deleted');
+    }
 
 }
