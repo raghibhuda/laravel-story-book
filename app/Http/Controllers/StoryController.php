9cdae7d54809dbef3796acfe4278fca813c33cb2
Ragib Ahsanul <raghibhuda@gmail.com>
2020-02-29 12:34:57 +0600

    Code clean up

--- app/Http/Controllers/StoryController.php
+++ app/Http/Controllers/StoryController.php
@@ -3,7 +3,12 @@
 namespace App\Http\Controllers;
 
 use App\Story;
+use Exception;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Routing\Redirector;
+use Illuminate\View\View;
 
 /**
  * Class StoryController
@@ -12,56 +17,92 @@ use Illuminate\Http\Request;
 class StoryController extends Controller
 {
     /**
-     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     * @return Factory|View
      */
-    public function index() {
-        $stories =  Story::all();
-        return view('stories')->with($stories);
+    public function index()
+    {
+        $stories = Story::all();
+        return view('stories', compact($stories));
     }
 
     /**
-     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     * @return Factory|View
      */
-    public function create () {
+    public function create()
+    {
         return view('create');
     }
 
     /**
      * @param Request $request
-     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
+     * @return RedirectResponse|Redirector
      */
-    public function store(Request $request) {
-        Story::create($request->all());
-        return redirect('/stories')->with('success', 'Story is successfully saved');
+    public function store(Request $request)
+    {
+        // Todo: add validation
+        try {
+            Story::create([
+                'title' => $request->title,
+                'body' => $request->body
+            ]);
+            return redirect('/stories')->with('success', 'Story is successfully saved');
+        } catch (Exception $e) {
+            return back()->with('error', 'Failed to create story');
+        }
+
     }
 
     /**
      * @param $id
-     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     * @return Factory|RedirectResponse|View
      */
-    public function edit ($id) {
+    public function edit($id)
+    {
         $story = Story::find($id);
-        return view('edit')->with($story);
+        if (!$story) {
+            return redirect()->back()->with('error', 'Story not found');
+        }
+        $data['story'] = $story;
+        return view('edit', $data);
     }
 
     /**
      * @param Request $request
-     * @param $id
-     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
+     * @return RedirectResponse|Redirector
      */
-    public function update (Request $request,$id) {
-        Story::whereId($id)->update($request->all());
-        return redirect('/stories')->with('success', 'Story is successfully saved');
+    public function update(Request $request)
+    {
+        // Todo: validation
+        try {
+            $story = Story::where('id', $request->id)->update([
+                'title' => $request->title,
+                'body' => $request->body
+            ]);
+            if (!$story) {
+                return back()->with('error', 'Story not found');
+            }
+            return redirect('/stories')->with('success', 'Story is successfully saved');
+        } catch (Exception $e) {
+            return back()->with('error', 'Something went wrong');
+        }
+
     }
 
     /**
      * @param $id
-     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
+     * @return RedirectResponse|Redirector
      */
-    public function destroy($id) {
-        $story = Story::findOrfail($id);
-        $story->delete();
-        return redirect('/stories')->with('success', 'Story is successfully deleted');
+    public function destroy($id)
+    {
+        try {
+            $story = Story::where('id', $id)->delete();
+            if (!$story) {
+                return back()->with('error', 'Story not found');
+            }
+            return redirect('/stories')->with('success', 'Story is successfully deleted');
+        } catch (Exception $e) {
+            return back()->with('error', 'Something went wrong');
+        }
     }
 
 }
